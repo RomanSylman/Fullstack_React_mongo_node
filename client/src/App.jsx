dc89c9b9141e0cb10bf51d2afb06d43d56e39eaa
Roman <woody.falcone.00@gmail.com>
2024-09-22 14:13:35 +0300

    app.jsx vite cfg

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,16 @@
 import axios from "axios";
-import {UserContextProvider} from "./UserContext";
+import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
+axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3030';
+axios.defaults.withCredentials = true;
+
 function App() {
-  axios.defaults.baseURL = 'http://localhost:3030';
-  axios.defaults.withCredentials = true;
   return (
     <UserContextProvider>
       <Routes />
     </UserContextProvider>
-  )
+  );
 }
 
-export default App
+export default App;
