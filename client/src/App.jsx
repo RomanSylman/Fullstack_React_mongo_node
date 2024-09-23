b7f9b83481e2294273a899acb6e2658a3bc79238
Roman <woody.falcone.00@gmail.com>
2024-09-23 18:57:31 +0300

    app.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import axios from "axios";
 import { UserContextProvider } from "./UserContext";
 import Routes from "./Routes";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL || 'http://localhost:3030';
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 axios.defaults.withCredentials = true;
 
 function App() {
