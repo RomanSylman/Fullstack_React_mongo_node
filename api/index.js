cb423e60b0c0050541280a435684ea42e60ec24a
Roman <woody.falcone.00@gmail.com>
2024-09-23 19:35:49 +0300

    api bug fixed

--- api/index.js
+++ api/index.js
@@ -114,9 +114,9 @@ app.post('/register', async (req,res) => {
 });
 
 const path = require('path');
-app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, 'client/dist')));
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    res.sendFile(path.join(__dirname, 'client/dist', 'index.html'));
 });
 
 
